4965deba246240df69b1cd858731bf884511753b
Sofia Hojberg <sofiahojberg@gmail.com>
2023-06-22 10:13:34 -0300

    Added routes

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/signup.html"))
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+});
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+});
+
 app.listen(PORT, () => console.log("Listening on http://localhost:" + PORT));
