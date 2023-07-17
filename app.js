c949dfe59b51a4522d2c4e410cfdaf8d0d8d3770
Sofia Hojberg <sofiahojberg@gmail.com>
2023-07-17 15:19:16 -0300

    Change port configuration

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const path = require('path');
 const mainRoutes = require('./routes/mainRoutes');
 const app = express();
 require('dotenv').config();
-const PORT = 3000;
-// const PORT = parseInt(process.env.PORT);
+const PORT = parseInt(process.env.PORT);
 
 app.set("view engine", "ejs")
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use('/', mainRoutes)
 
-app.listen(PORT, () => console.log("Listening on http://localhost:" + PORT));
+app.listen(PORT, () => console.log("Listening on http://localhost:" + PORT))
