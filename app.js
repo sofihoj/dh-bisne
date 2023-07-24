d357c447fef0937e7da25b99b5a38afec476d13d
Sofia Hojberg <sofiahojberg@gmail.com>
2023-07-24 20:56:30 -0300

    Designed admin products table

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mainRoutes = require('./routes/mainRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 require('dotenv').config();
 const PORT = parseInt(process.env.PORT);
@@ -9,6 +10,7 @@ app.set("view engine", "ejs")
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use('/', mainRoutes)
+app.use('/administrar', adminRoutes)
 
 app.use((req,res,next)=>{
     res.status(404).render('not-found')
